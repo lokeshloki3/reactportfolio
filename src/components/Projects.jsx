6906f8be5a80f11a3524773ba537b0c24649ae6f
lokeshloki3 <lokesh1992p@gmail.com>
2024-10-03 13:06:47 +0530

    Github Pages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,38 +8,40 @@ const Projects = () => {
             Projects
         </h2>
         <div>
-            {PROJECTS.map((project, index) => (
-                <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-                    <div className='w-full lg:w-1/4'>
-                        <a href={project.githubPage} target='_blank'>
-                            <img src={project.image}
-                                width={250}
-                                height={250}
-                                alt={project.title}
-                                className='mb-6 rounded'
-                            />
-                        </a>
+            {
+                PROJECTS.map((project, index) => (
+                    <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
+                        <div className='w-full lg:w-1/4'>
+                            <a href={project.githubPage} target='_blank'>
+                                <img src={project.image}
+                                    width={250}
+                                    height={250}
+                                    alt={project.title}
+                                    className='mb-6 rounded'
+                                />
+                            </a>
+                        </div>
+                        <div className='w-full max-w-xl lg:w-3/4'>
+                            <a href={project.githubPage} target='_blank'>
+                                <h3 className='mb-2 font-semibold text-2xl'>
+                                    {project.title}
+                                </h3>
+                            </a>
+                            <p className='mb-4 text-stone-400'>
+                                {project.description}
+                            </p>
+                            <p>
+                                {project.technologies.map((tech, index) => (
+                                    <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'
+                                    key={index}>
+                                        {tech}
+                                    </span>
+                                ))}
+                            </p>
+                        </div>
                     </div>
-                    <div className='w-full max-w-xl lg:w-3/4'>
-                        <a href={project.githubPage} target='_blank'>
-                            <h3 className='mb-2 font-semibold text-2xl'>
-                                {project.title}
-                            </h3>
-                        </a>
-                        <p className='mb-4 text-stone-400'>
-                            {project.description}
-                        </p>
-                        <p>
-                            {project.technologies.map((tech, index) => (
-                                <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300'
-                                key={index}>
-                                    {tech}
-                                </span>
-                            ))}
-                        </p>
-                    </div>
-                </div>
-            ))}
+                ))
+            }
         </div>
     </div>
   )
