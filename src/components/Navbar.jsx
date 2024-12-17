ae514c28deb0148cfaadb08a7d7c1b68f6229ae3
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-18 00:46:21 +0530

    mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,34 @@ import logo from "../assets/LWLogo-removebg-preview.png"
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between py-6 border-b border-stone-700'>
+    <nav className='flex flex-col sm:flex-row items-center justify-between py-6 border-b border-stone-700'>
       <div className='flex flex-shrink-0 items-center'>
         <a href='/reactportfolio/' aria-label='Home'>
           <img src={logo} className="mx-2 rounded-xl" width={100} height={33} alt="Logo"/>
         </a>
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="m-8 flex flex-col sm:flex-row items-center justify-center gap-4 text-2xl">
         <a href="#technologies">Technologies</a>
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
-        <a href="https://www.linkedin.com/in/lokesh021loki/"
+        <div className="flex flex-row gap-4">
+          <a
+            href="https://www.linkedin.com/in/lokesh021loki/"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="LinkedIn">
-          <FaLinkedin/>
-        </a>
-        <a href="https://github.com/lokeshloki3/"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/lokeshloki3/"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="GitHub">
-          <FaGithub/>
-        </a>
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
+        </div>
       </div>
     </nav>
   )
